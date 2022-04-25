e1cbabda87f4b2f8b6a604802599957d8b93fb93
Najib Hasan <hasan1807080@stud.kuet.ac.bd>
2022-04-25 20:12:27 +0600

    Front end part of my project

---
+++ JS/main.js
@@ -0,0 +1,64 @@
+
+
+/*-----------------
+ toggle navbar
+-------------------*/
+
+
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+// console.log(navToggler)
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+
+
+}
+
+
+
+// close Nav bar
+document.addEventListener("click", function(e){
+    if(e.target.closest(".nav-item")){
+        toggleNav();
+    }
+})
+
+
+
+
+/* -----------------
+Sticky header
+-------------------*/
+
+window.addEventListener("scroll",function(){
+    if(this.pageYOffset > 60){
+        document.querySelector(".header").classList.add("sticky");
+        text = document.querySelector(".resName");
+        text.style.color = "#eaa023"
+    }
+    else{
+        document.querySelector(".header").classList.remove("sticky");
+        text = document.querySelector(".resName");
+        text.style.color = "white"
+        
+    }
+});
+
+
+/*----------------------menu tabs ----------------------------*/
+
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click",function(e){
+    if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+        const target = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+    }
+});
